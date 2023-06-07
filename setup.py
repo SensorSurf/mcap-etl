9387aae2a94512e69fabd4ccaa470e332bd0fb99
Alec Bell <alec.timothy.bell@gmail.com>
2023-06-07 10:32:18 -0700

    Name format change, read requirements.txt

--- setup.py
+++ setup.py
@@ -6,14 +6,17 @@ from setuptools import setup, find_packages
 with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+with open('requirements.txt') as file:
+    requirements = file.read().splitlines()
+
 setup(
-    name='mcap_etl',
+    name='mcap-etl',
     author='SensorSurf',
     author_email='support@sensorsurf.com',
     description='Transform mcap (or rosbag) files into databases or other files',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/SensorSurf/mcap_etl',
+    url='https://github.com/SensorSurf/mcap-etl',
     packages=find_packages(),
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
@@ -28,30 +31,14 @@ setup(
         'Programming Language :: Python :: 3.9',
     ],
     keywords='ros, ros2, rosbag, mcap, timescale, etl, timeseries, database, etl',
-    install_requires=[
-        'jmespath==1.0.1',
-        'lz4==4.3.2',
-        'mcap==1.0.2',
-        'numpy==1.23.1',
-        'pandas==2.0.1',
-        'psycopg2-binary==2.9.6',
-        'python-dateutil==2.8.2',
-        'pytz==2023.3',
-        'rosbags==0.9.15',
-        'ruamel.yaml==0.17.26',
-        'ruamel.yaml.clib==0.2.7',
-        'six==1.16.0',
-        'tzdata==2023.3',
-        'urllib3==1.26.15',
-        'zstandard==0.21.0',
-    ],
+    install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'mcap_etl=mcap_etl.main:main',
+            'mcap-etl=mcap_etl.main:main',
         ],
     },
     project_urls={
-        'Bug Reports': 'https://github.com/SensorSurf/mcap_etl/issues',
-        'Source': 'https://github.com/SensorSurf/mcap_etl',
+        'Bug Reports': 'https://github.com/SensorSurf/mcap-etl/issues',
+        'Source': 'https://github.com/SensorSurf/mcap-etl',
     },
 )
